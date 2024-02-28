053be5655cd627f3c421b8d927eed4fd6b8cc42c
Jaime Avilleira Garcia <79282269+avilleira@users.noreply.github.com>
2024-02-28 17:00:20 +0100

    Possible solution for the csv data split

--- src/signal_plot.py
+++ src/signal_plot.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+
 def get_data(data_path):
     signal_data = pd.read_csv(data_path)
+    # signal_data.iloc[:, 9:16] # Para poder realizar el split en los diferentes csv
     
