bf4c3a9a2055a77623eb64a9dada85e74c1e97d5
avilleira <j.avilleira.2019@alumnos.urjc.es>
2024-03-01 19:43:19 +0100

    sEMG and time data bounded. Needs to plot it
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- src/signal_plot.py
+++ src/signal_plot.py
@@ -3,7 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 
-# Macros
+# MACROS
+
 EXIT_FAILURE = 1
 ARGS_N = 3
 DATASET_PATH = '../../dataset/SIAT_LLMD20230404'
@@ -14,36 +15,46 @@ def usage():
     Usage error exit
     """
 
-    sys.stderr.write("Usage: python3 signal_plot.py subject_name muscle\n")
+    sys.stderr.write("Usage: python3 signal_plot.py SubXX action muscle/all\n")
     sys.exit(EXIT_FAILURE)
 
 
-def get_path(subject, muscle):
+def get_sEMG_data(subject, action_str):
     """
-    This function returns the complete route to the dataset
+    This function returns the bounded Dataframe with the sEMG data and the time
+    stamp of all the data points.
+
+    :param subject: Name of the subject whose data comes from
+    :type: string
+    :param action_str: Action that the subject is doing
+    :type: string
+    :return: The bounded dataframe
+    :rtype: pd.Dataframe
     """
 
-    muscle += '.csv'
-    return  os.path.join(DATASET_PATH, subject, 'Data', muscle)
-
-
-def get_sEMG_data(data_path):
-    """
-    This function returns the bounded Dataframe
-    """
-
+    data_file = subject + '_' + action_str + '_Data.csv'
+    # Creating the path
+    data_path = os.path.join(DATASET_PATH, subject, 'Data', data_file)
+    # Creating the DataFrame
     signal_data = pd.read_csv(data_path)
-    signal_data.iloc[:, 9:16] # Para poder realizar el split en los diferentes csv
-    
+
+    # Selecting the sEMG data and the time stamp
+    time_df = signal_data.iloc[:, 0:1]
+    semg_df = signal_data.filter(regex='^sEMG: ')
+
+    # Creating bounded Dataframe. It concatenates in the cols axis
+    bounded_df = pd.concat([time_df, semg_df], axis=1)
+
+    return bounded_df
+
 
 def main():
 
     if len(sys.argv) != ARGS_N:
         usage()
 
-    sub_path = get_path(sys.argv[1], sys.argv[2])
-    print(sub_path)
-
+    semg_df = get_sEMG_data(sys.argv[1], sys.argv[2])
+    print(semg_df)
 
 if __name__ == "__main__":
     main()
