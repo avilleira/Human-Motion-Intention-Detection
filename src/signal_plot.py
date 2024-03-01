620cc51e45922fae539fdff2a233341c691d90a7
avilleira <j.avilleira.2019@alumnos.urjc.es>
2024-03-01 01:56:21 +0100

    The program receives arguments from the line command and shows the posible path
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- src/signal_plot.py
+++ src/signal_plot.py
@@ -1,8 +1,49 @@
+import sys
+import os
 import pandas as pd
 import numpy as np
 
+# Macros
+EXIT_FAILURE = 1
+ARGS_N = 3
+DATASET_PATH = '../../dataset/SIAT_LLMD20230404'
 
-def get_data(data_path):
+
+def usage():
+    """
+    Usage error exit
+    """
+
+    sys.stderr.write("Usage: python3 signal_plot.py subject_name muscle\n")
+    sys.exit(EXIT_FAILURE)
+
+
+def get_path(subject, muscle):
+    """
+    This function returns the complete route to the dataset
+    """
+
+    muscle += '.csv'
+    return  os.path.join(DATASET_PATH, subject, 'Data', muscle)
+
+
+def get_sEMG_data(data_path):
+    """
+    This function returns the bounded Dataframe
+    """
+
     signal_data = pd.read_csv(data_path)
-    # signal_data.iloc[:, 9:16] # Para poder realizar el split en los diferentes csv
+    signal_data.iloc[:, 9:16] # Para poder realizar el split en los diferentes csv
     
+
+def main():
+
+    if len(sys.argv) != ARGS_N:
+        usage()
+
+    sub_path = get_path(sys.argv[1], sys.argv[2])
+    print(sub_path)
+
+
+if __name__ == "__main__":
+    main()
