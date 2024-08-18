10d5b7a9ec828dcd98df027c379cad39f8ac6f2f
avilleira <j.avilleira.2019@alumnos.urjc.es>
2024-08-18 02:47:06 +0200

    Trying a training
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- src/svm_sEMG.py
+++ src/svm_sEMG.py
@@ -2,8 +2,10 @@ import ast
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 TRAIN_DATA_PATH = '/home/avilleira/TFG/tfg/data/training_data.csv'
+DU_SET_LIST = ['iemg', 'variance', 'waveform length', 'zero crossing', 'slope sign change', 'willison amplitude']
 
 
 def is_str_list(str_list):
@@ -21,8 +23,22 @@ def str_to_column(col):
         return ast.literal_eval(col)
     return col
 
+def flatten_column(df, col):
+        # Convertir la columna en listas si es necesario
+    df[col] = df[col].apply(str_to_column)
+    
+    # Si la columna contiene listas, expandirlas en nuevas columnas
+    if df[col].apply(lambda x: isinstance(x, list)).any():
+        flattened_df = pd.DataFrame(df[col].tolist(), index=df.index)
+        flattened_df.columns = [f"{col}_{i}" for i in range(flattened_df.shape[1])]
+        return flattened_df
+    else:
+        return df[[col]]
+
 
 def main():
+
+    flattened_columns = []
     
     # Reading from the csv file:
     data_svm = pd.read_csv(TRAIN_DATA_PATH)
@@ -32,15 +48,25 @@ def main():
         if data_svm[col].apply(is_str_list).any():
             data_svm[col] = data_svm[col].apply(str_to_column)
     
-    print(data_svm.head())
+    # Flat columns:
+    print("Flattening cols")
+    for col in DU_SET_LIST:
+        flattened_columns.append(flatten_column(data_svm, col))
     # Let's start with a Linear kernel:
-    #svm_mdl = svm.SVC(kernel="poly")
+    svm_mdl = svm.SVC(kernel="poly")
 
-    X = data_svm[['iemg','variance','waveform length','zero crossing','slope sign change','willison amplitude']]
+    X = pd.concat(flattened_columns, axis=1)
     Y = data_svm['output']
-    #print(X)
+
     # Model training
-    #svm_mdl.fit(X, Y)
+    print("Starting training")
+    svm_mdl.fit(X, Y)
+
+    # Salida predicha
+    y_pred = svm_mdl.predict(X)
+    # Error en la precisión
+    err = 1 - (accuracy_score(Y, y_pred))
+    print(f"ERROR: {err}")
 
 
 if __name__ == "__main__":
